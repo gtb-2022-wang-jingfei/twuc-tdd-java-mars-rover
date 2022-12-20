472013afac5781538cf5d35b85e2764827c34866
Songlin Li <slli@thoughtworks.com>
2022-12-20 23:25:59 +0800

    init test

---
+++ src/test/java/com/twuc/marsrover/MarsRoverTest.java
@@ -0,0 +1,12 @@
+package com.twuc.marsrover;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MarsRoverTest {
+
+    @Test
+    void shouldXxxWhenYyy() {
+        Assertions.assertTrue(1 == 1);
+    }
+}
